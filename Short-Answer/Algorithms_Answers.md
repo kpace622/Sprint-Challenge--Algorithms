966c2f4deedef4f190b5ac739ef215d41d68382a
Kyle Pace <kpace2296@gmail.com>
2020-10-09 20:26:47 -0500

    finished recursive count for now

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,16 @@
 
 ## Exercise I
 
-a)
+a) # O(n) due to the size of n or would what it be O(log n) due to n being multiplied each iteration
 
 
-b)
+
+b) # O(n * log n) while loop would be log n because the number is doubling every iteration otherwise the whole function would be O(n ^ 2)? 
 
 
-c)
+
+c) # O(n) recursive function that runs n time where n is bunnies 
 
 ## Exercise II
 
-
+# Binary search. I would pick the floor in the middle of the building, throw an egg. If the egg breaks, pick a new middle floor where the current floor becomes the top. If the egg doesn't break pick a new middle floor higher up where the bottom would be the floor you are currently on. Repeat until you pinpoint the highest floor where the egg doesn't break.
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,34 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+
+    # Turn the word into a list
+    arr = list(word)
+
+    # Declare starting indexes to inspect the list
+    idx_one = 0
+    idx_two = 1
+
+    # Keep count of how many times 'th' appears
+    count = 0
+    print(idx_one)
+
+    # Base Case, If you get to the end of the list stop the recursion
+    if arr[idx_two] is len(arr) -1:
+        return count
+
+    # If the two indexes fulfill this requirement increment the count and then recall the function using + 1 on both indexes 
+    if arr[idx_one == 't'] and arr[idx_two == 'h']:
+        count += 1
+        idx_one += 1
+        idx_two += 1
+        return count_th(word)
+
+    # Otherwise continue moving through the array, incrementing the indexes
+    else: 
+        idx_one += 1
+        idx_two += 1
+        return count_th(word)
